52119541883e3abb865ca05bcd5e40007663850a
danielfirst98 <danielfirst98@gmail.com>
2026-09-03 11:27:15 +0900

    Change file location about Modal.jsx

--- src/components/publicComponents/Modal.jsx
+++ src/components/publicComponents/Modal.jsx
@@ -1,5 +1,5 @@
 import { createPortal } from "react-dom";
-import styles from "../styles/Modal.module.css";
+import styles from "#styles/Modal.module.css";
 
 function Modal({variant, isOpen, onClose, children}) {
   if (!isOpen) {
