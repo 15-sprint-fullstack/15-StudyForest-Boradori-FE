765bb1fcee0ddf74cbdb8314ad2fb83558b6369a
Seunghyun Lee <mona1222@naver.com>
2026-09-03 09:53:34 +0900

    refactor: 모달 내부 children으로 통합, css 기본만 남기고 분리

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,39 +1,41 @@
-import { createPortal } from "react-dom";
-import styles from "../styles/Modal.module.css";
+import { createPortal } from 'react-dom';
+import styles from '../styles/Modal.module.css';
 
-function Modal({variant, isOpen, onClose, children}) {
+function Modal({ isOpen, onClose, children }) {
   if (!isOpen) {
     return null;
   }
 
   return createPortal(
     <div className={styles.overlay}>
-      <div className={`${styles.modal} ${styles[variant]}`}>
-        <button onClick={onClose}>닫기</button>
+      <div className={`${styles.modal} ${modalClassName ?? ''}`}>
+        {' '}
+        //modalClassName은 각자 정하고 담당 컴포넌트 css파일에서 스타일 주기.
         {children}
       </div>
     </div>,
-    document.getElementById("modal-root"),
+    document.getElementById('modal-root'),
   );
 }
 
 export default Modal;
 
-// 실제 사용하실 때에는 아래거 붙여넣기 하셔서 모달 내의 요소들(text, input, button 등) 넣으시고 사용하시면 됩니다!
-
 // 숙연님께.
+// 실제 사용하실 때에는 아래거 붙여넣기 하셔서 모달 내의 요소들(text, input, button 등) 넣으시고 사용하시면 됩니다!
+//
 // 컴포넌트 본문에는 const [isPasswordModalOpen, setIsPasswordModalOpen] = useState(false); 추가
 // 모달을 열게 만들 요소에는 onClick={() => setIsPasswordModalOpen(true)}} 속성 추가
-// return 부분 원하시는 위치에(위치상관x) 아래 모달 추가 후 태그 사이에 각 요소 배치
-// <Modal variant="password"
-//        isOpen={isPasswordModalOpen}
+// return 부분 원하시는 위치에(위치상관x) 아래 모달 추가 후 {children} 부분에 각 요소 배치
+// {children} 내부 나가기 버튼에 onClick={onClose} 속성 추가하여 사용
+//
+// <Modal isOpen={isPasswordModalOpen}
 //        onClose={() => setIsPasswordModalOpen(false)}>
+// {children}
 // </Modal>
 
 // 이건 제가 쓸 거.
 // const [isHabitEditModalOpen, setIsHabitEditModalOpen] = useState(false);
-// <Modal variant="habitEdit"
-//        isOpen={isHabitEditModalOpen}
+// <Modal isOpen={isHabitEditModalOpen}
 //        onClose={() => setIsHabitEditModalOpen(false)}>
+// {children}
 // </Modal>
-
