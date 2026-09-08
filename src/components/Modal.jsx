39fa237d37ebc7cbe13b6fbf0894e103456314a0
Seunghyun <mona1222@naver.com>
2026-09-08 19:41:10 +0900

    feature: 오늘의 습관 페이지 골격 및 습관 API 제작

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,8 +8,8 @@ function Modal({ isOpen, children }) {
 
   return createPortal(
     <div className={styles.overlay}>
-      <div className={`${styles.modal} ${modalClassName ?? ''}`}>
-        //modalClassName은 각자 정하고 담당 컴포넌트 css파일에서 스타일 주기.
+      <div className={`${styles.modal} ${styles.modalClassName}`}>
+        {/* modalClassName은 각자 정하고 담당 컴포넌트 css파일에서 스타일 주기. */}
         {children}
       </div>
     </div>,
