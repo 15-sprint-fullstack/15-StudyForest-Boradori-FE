c3d262ed36ada1799af00c0151d599b3368cf1b9
sookyeon <285711703+sswapbot@users.noreply.github.com>
2026-09-08 16:48:55 +0900

    chore: StudyDetail에서 HabitRecordTable의 CSS 분리

--- src/pages/StudyDetailPage/HabitRecordTable.jsx
+++ src/pages/StudyDetailPage/HabitRecordTable.jsx
@@ -1,4 +1,6 @@
-import styles from '../../pages/StudyDetailPage/StudyDetailPage.module.css';
+import styles from './HabitRecordTable.module.css'; // 습관 기록표 전용 스타일을 불러와요.
+import completeIcon from '../../assets/stickers/sticker-light-green-100-03.svg'
+import emptyIcon from '../../assets/stickers/sticker-inactive.svg';
 
 export function HabitRecordTable({ habits }) {
   return (
@@ -32,7 +34,12 @@ export function HabitRecordTable({ habits }) {
                   <th scope="row">{habit.name}</th>
                   {habit.records.map((isCompleted, dayIndex) => (
                     <td key={dayIndex}>
-                      {isCompleted ? '완료' : '-'}
+                      <img
+                         className={styles.recordIcon}
+                         src={isCompleted ? completeIcon : emptyIcon}
+                         width={36}
+                         height={36}
+                      />
                     </td>
                   ))}
                 </tr>
